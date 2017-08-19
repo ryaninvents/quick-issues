deb34b67c9b88d10f4a74188be39b3f5b7ee84a7
Sebastain Gerhardt <oss@bittracker.org>
2017-08-19 16:45:35 +0200

    Finxing: r24y/quick-issues#2
    This quick and dirty Hack fixes the SSH issue and adding some Documentation
    about the API-Token permissions!

--- src/find-repos.js
+++ src/find-repos.js
@@ -4,12 +4,12 @@ import * as urlUtils from 'url';
 export default async function findReposInWorkspace() {
     const repo = atom.project.getRepositories().find((repo) => {
         if (!repo || !repo.getOriginURL()) return false;
-        const remote = urlUtils.parse(repo.getOriginURL());
+        const remote = urlUtils.parse(repo.getOriginURL().replace(":","/").replace("git@","https://"));
         if (remote.host !== 'github.com') return false;
         return true;
     });
     if (!repo) return null;
-    const remote = urlUtils.parse(repo.getOriginURL());
+    const remote = urlUtils.parse(repo.getOriginURL().replace(":","/").replace("git@","https://"));
     const [user, repoName] = remote.path.split('/').filter(Boolean).slice(0, 2);
     return {user, name: repoName.replace(/\.git$/, '')};
 }
