4592b33a51980ddea85ce591115d731d631b98be
Ryan Kennedy <ryan@rmkennedy.com>
2017-08-25 10:51:18 -0400

    Break SSH => HTTPS into function
    Also limit this translation to only GitHub repos for now

--- src/find-repos.js
+++ src/find-repos.js
@@ -1,15 +1,22 @@
 /** @babel */
 import * as urlUtils from 'url';
 
+/**
+ * If this repo is a GitHub SSH repo, translate the URL to an HTTPS URL.
+ */
+function convertSshToHttps(url) {
+    return url.replace(/^git@github.com:/, 'https://github.com/');
+}
+
 export default async function findReposInWorkspace() {
     const repo = atom.project.getRepositories().find((repo) => {
         if (!repo || !repo.getOriginURL()) return false;
-        const remote = urlUtils.parse(repo.getOriginURL());
+        const remote = urlUtils.parse(convertSshToHttps(repo.getOriginURL()));
         if (remote.host !== 'github.com') return false;
         return true;
     });
     if (!repo) return null;
-    const remote = urlUtils.parse(repo.getOriginURL());
+    const remote = urlUtils.parse(convertSshToHttps(repo.getOriginURL()));
     const [user, repoName] = remote.path.split('/').filter(Boolean).slice(0, 2);
     return {user, name: repoName.replace(/\.git$/, '')};
 }
